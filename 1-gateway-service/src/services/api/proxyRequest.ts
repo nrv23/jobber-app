d3192617dea73cfa53e5ee70fd7b625a922b1dec
nrv2391 <navemen23@hotmail.com>
2024-12-17 22:41:16 -0600

    ajustar el manejo del error en el proxyRequest Method

--- 1-gateway-service/src/services/api/proxyRequest.ts
+++ 1-gateway-service/src/services/api/proxyRequest.ts
@@ -2,11 +2,16 @@
 
 import axios, { AxiosRequestConfig, Method } from 'axios';
 import { config } from '@gateway/config';
+import BaseError from '@gateway/utils/Error';
+import { Logger } from 'winston';
+import { winstonLogger } from '@nrv23/jobber-shared';
 
 export const services: Record<string, string> = {
   auth: config.configProperties.AUTH_BASE_URL!,
   notifications: config.configProperties.NOITFICATION_BASE_URL!,
 };
+
+const log: Logger = winstonLogger(`${config.configProperties.ELASTIC_SEARCH_URL}`, 'Authentication Server', 'debug');
 
 /**
  * Proxy dinámico para redirigir solicitudes al microservicio adecuado.
@@ -32,9 +37,20 @@ class ProxyService {
       const response = await axios.request(config);
       return response;
     } catch (error) {
-      console.error(`Error en la solicitud ${method} a ${targetUrl}:`, error);
+      const customeError = this.handleError(error);
+      log.log('error', `AuthenticationService proxyRequest method() ${customeError.getError().comingFrom}`, customeError);
       throw error;
     }
+  }
+
+  private handleError(error: any) {
+    const customeError = new BaseError({
+      message: error.message || 'Error desconocido',
+      statusCode: error.response?.status || 500,
+      commingFrom: 'gateway Service proxyRequest() Method'
+    });
+
+    return customeError;
   }
 }
 
